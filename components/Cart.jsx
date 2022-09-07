3c38332f3c622a5bba832d9f6e16aba319b879b8
Andrew Zihao Li <zliwow123@gmail.com>
2022-09-07 16:07:06 -0700

    modified:   components/Cart.jsx 	modified:   components/FooterBanner.jsx 	modified:   components/HeroBanner.jsx 	modified:   components/Navbar.jsx 	modified:   context/StateContext.js 	new file:   lib/getStripe.js 	new file:   lib/utils.js 	new file:   pages/api/stripe.js 	new file:   pages/successs.js

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -5,9 +5,28 @@ import { TiDeleteOutline} from 'react-icons/ti';
 import toast from 'react-hot-toast';
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
+import getStripe from '../lib/getStripe';
 const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove} = useStateContext();
+
+  const handleCheckOut = async() => {
+    const stripe = await getStripe();
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+    if(response.statusCode === 500) return;
+
+    const data = await response.json();
+    toast.loading('Redirecting...');
+
+    stripe.redirectToCheckout({sessionId: data.id});
+  }
   return (
     <div className='cart-wrapper' ref={cartRef}>
       <div className='cart-container'>
@@ -73,7 +92,7 @@ const Cart = () => {
             </div>
             <div className='btn-container'>
               <button type='button' className='btn'
-              onClick=''>
+              onClick={handleCheckOut}>
                 Pay with Stripe
               </button>
 
