e962ddf28f0f960ee723fa3bd216546cba29c4a3
Andrew Zihao Li <zliwow123@gmail.com>
2022-09-07 21:30:56 -0700

    modified:   components/Footer.jsx 	modified:   components/Layout.jsx 	modified:   components/Navbar.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <p>2022 Andrew's Collections All rights reserved</p>
+      <p>2022 Andrew&apos;s Collections All rights reserved</p>
       <p className='icons'>
         <AiFillInstagram />
         <AiOutlineTwitter />
